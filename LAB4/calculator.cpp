0bd21841222d9a8e507dcc7b9c3ee51a347d5356
NEWWAYUA <60075613+NEWWAYUA@users.noreply.github.com>
2020-01-19 22:39:15 +0200

    git add

---
+++ LAB4/calculator.cpp
@@ -0,0 +1,10 @@
+#include "/home/kenplix/calculator/calculator.h"
+double Calculator::Add (double a, double b)
+{
+	return a + b + 0.5;
+}
+
+int Calculator::Sub (double a, double b)
+{
+	return Add (a, -b);
+}
